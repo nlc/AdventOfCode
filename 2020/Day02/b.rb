d6e87655db13dcbe680472c705a19490b66eefd3
Nate <lander@kicksite.net>
2020-12-01 23:33:32 -0600

    refactor a bit

--- 2020/Day02/b.rb
+++ 2020/Day02/b.rb
@@ -2,14 +2,14 @@ total = 0
 File.readlines('input.txt', chomp: true).each do |line|
   nums, letter, string = line.split
 
-  least, most = nums.split('-').map(&:to_i)
+  indices = nums.split('-').map(&:to_i)
   char = letter.chars[0]
 
-  num_matches = 0
+  num_matches =
+    indices.count do |index|
+      string[index - 1] == char
+    end
 
-  num_matches += 1 if (string.chars[least-1] == char) && (string.chars[most - 1] != char)
-  num_matches += 1 if (string.chars[least-1] != char) && (string.chars[most - 1] == char)
-
-  total += 1 if num_matches == 1
+  total += num_matches % 2
 end
 puts total
