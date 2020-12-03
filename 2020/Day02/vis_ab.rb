aee33ae844232f2657fd350bf04e2ce9f9f68c2d
Nate <lander@kicksite.net>
2020-12-03 05:47:55 -0600

    add more vis to Day02, begin Day03

---
+++ 2020/Day02/vis_ab.rb
@@ -0,0 +1,85 @@
+def slowprint(str)
+  str.chars.each do |char|
+    print char
+    sleep 0.02
+  end
+end
+
+total = 0
+File.readlines('input.txt', chomp: true).each do |line|
+  nums, letter, string = line.split
+
+  least, most = nums.split('-').map(&:to_i)
+  char = letter.chars[0]
+
+  count = string.count(char)
+  too_few = count < least
+  too_many = count > most
+  found = !too_few && !too_many
+  total += 1 if found
+
+  indices = (0...string.length).to_a.select { |i| string[i] == char }
+  slowprint sprintf("%-5s \033[1m%c\033[0m: %s", nums, char, string)
+  print "\033[#{string.length}D"
+  last_index = 0
+  indices.each_with_index do |index, i|
+    sleep 0.1
+    dx = index - last_index
+    if dx > 0
+      print "\033[#{dx}C"
+    end
+
+    i += 1
+    if i < least
+      print "\033[34m"
+    elsif i > most
+      print "\033[31m"
+    else
+      print "\033[32m"
+    end
+
+    print char
+    print "\033[0m"
+    print "\033[D"
+
+    last_index = index
+  end
+  sleep 0.2
+  print "\r\033[32C"
+  if too_few
+    slowprint "\033[34mTOO FEW (#{count} < #{least})\033[0m"
+  elsif too_many
+    slowprint "\033[31mTOO MANY (#{count} > #{most})\033[0m"
+  else
+    slowprint "\033[32mVALID (#{least} <= #{count} <= #{most})\033[0m"
+  end
+
+  print "\r\033[56C"
+  slowprint string
+  print "\033[#{string.length}D"
+  last_index = 0
+  num_matches = 0
+
+  i1 = least - 1
+  i2 = most - 1
+  print "\r\033[#{56 + i1}C"
+  sleep 0.2
+  print "\033[1m#{string[i1]}\033[0m"
+  print "\r\033[#{56 + i2}C"
+  sleep 0.2
+  print "\033[1m#{string[i2]}\033[0m"
+
+  sleep 0.2
+  print "\r\033[78C"
+  if too_few
+    slowprint "\033[34mTOO FEW (#{count} < #{least})\033[0m"
+  elsif too_many
+    slowprint "\033[31mTOO MANY (#{count} > #{most})\033[0m"
+  else
+    slowprint "\033[32mVALID (#{count})\033[0m"
+  end
+
+  puts
+end
+
+puts total
