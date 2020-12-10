35d048af9006e8e54e2b602118a9b78bcfac84d7
Nate <lander@kicksite.net>
2020-12-10 16:55:26 -0600

    udpate solution and add a test of a memo header

--- 2020/Day10/memo_test.rb
+++ 2020/Day10/memo_test.rb
@@ -1,56 +1,38 @@
-require 'pp'
+# Test if the quickie "memo.rb" include will perform the
+# same as manually memoizing.
+# Turns out it's almost exactly the same performance in this case.
 
-# return a list of all chains that start with <adapter>
-def chain(adapter, neighbors)
-  {
-    adapter =>
-    if neighbors.key? adapter
-      neighbors[adapter].map do |neighbor|
-        chain(neighbor, neighbors)
-      end
-    end
-  }
-end
+require '../../utilities/memo.rb'
 
 # return the chain that starts with <adapter> and makes it to <target>
-$full_chain_memo = {}
 def full_chain(adapter, target, neighbors)
-  # Memoize
-
   return [adapter] if adapter == target
 
-  if $full_chain_memo.key?([adapter, target, neighbors])
-    $full_chain_memo[[adapter, target, neighbors]]
-  elsif neighbors.key? adapter
+  if neighbors.key? adapter
     results =
       neighbors[adapter].map do |neighbor|
-        full_chain(neighbor, target, neighbors)
+        _memo(:full_chain, neighbor, target, neighbors)
       end
     result = [adapter] + results.max_by { |r| r.length }
-    $full_chain_memo[[adapter, target, neighbors]] = result
     result
   end
 end
 
-$count_chain_memo = {}
 def count_chain(adapter, target, neighbors)
-  # Memoize
   return 1 if adapter == target
 
-  if $count_chain_memo.key?([adapter, target, neighbors])
-    $count_chain_memo[[adapter, target, neighbors]]
-  elsif neighbors.key? adapter
+  if neighbors.key? adapter
     results =
       neighbors[adapter].map do |neighbor|
-        count_chain(neighbor, target, neighbors)
+        _memo(:count_chain, neighbor, target, neighbors)
       end
     result = results.inject(:+)
-    $count_chain_memo[[adapter, target, neighbors]] = result
     result
   end
 end
 
-test_raw_adapters = File.readlines('temp.txt', chomp: true)[73..103].map(&:to_i)
+test1_raw_adapters = File.readlines('puzzle.txt', chomp: true)[32..42].map(&:to_i)
+test2_raw_adapters = File.readlines('puzzle.txt', chomp: true)[73..103].map(&:to_i)
 raw_adapters = File.readlines('input.txt', chomp: true).map(&:to_i)
 
 # An adapter is allowed to connect to another adapter
@@ -97,9 +79,12 @@ def part_b(raw_adapters)
 end
 
 puts "\033[1mPART A:\033[0m"
-test_counts_a = part_a(test_raw_adapters)
-test_ans_a = test_counts_a.values.inject(:*)
-puts "  TEST:   #{test_counts_a} --> \033[1m#{test_ans_a}\033[0m"
+test1_counts_a = part_a(test1_raw_adapters)
+test2_counts_a = part_a(test2_raw_adapters)
+test1_ans_a = test1_counts_a.values.inject(:*)
+test2_ans_a = test2_counts_a.values.inject(:*)
+puts "  TEST 1: #{test1_counts_a} --> \033[1m#{test1_ans_a}\033[0m"
+puts "  TEST 2: #{test2_counts_a} --> \033[1m#{test2_ans_a}\033[0m"
 
 counts_a = part_a(raw_adapters)
 ans_a = counts_a.values.inject(:*)
@@ -107,5 +92,6 @@ puts "  ACTUAL: #{counts_a} --> \033[1m#{ans_a}\033[0m"
 puts
 
 puts "\033[1mPART B:\033[0m"
-puts "  TEST:   \033[1m#{part_b(test_raw_adapters)}\033[0m"
+puts "  TEST 1: \033[1m#{part_b(test1_raw_adapters)}\033[0m"
+puts "  TEST 2: \033[1m#{part_b(test2_raw_adapters)}\033[0m"
 puts "  ACTUAL: \033[1m#{part_b(raw_adapters)}\033[0m"
---
+++ utilities/memo.rb
@@ -0,0 +1,19 @@
+$memo_table = {}
+
+def _memo_init()
+  $memo_table = {}
+end
+
+def _memo(method, *args)
+  memo_key = [method, args]
+  return $memo_table[memo_key] if $memo_table.key? memo_key
+  $memo_table[memo_key] = send(method, *args)
+end
+
+# Note that this implementation is crude. _memo() will have to wrap EVERY
+# call to self in a recursive function in order for this to work, e.g.
+#   def fib2(x)
+#     return x if x <= 3
+#     _memo(:fib2, x - 1) + _memo(:fib2, x - 2)
+#   end
+# There's no fancy metaprogramming here to "memo-ify" a pre-existing method.
