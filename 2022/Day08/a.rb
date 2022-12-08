329ec728030ffa5e7f5f34584b0c0b6760b69da2
nlc <natechav1@gmail.com>
2022-12-08 00:15:34 -0600

    clean up a.rb slightly

--- 2022/Day08/a.rb
+++ 2022/Day08/a.rb
@@ -13,42 +13,38 @@ num_visible = 0
     visibility = []
 
     biggest_rightward = trees[y][(x+1)..].max || 0
-    p biggest_rightward
     if tree > biggest_rightward
       is_visible = true
-      visibility << 'RIGHT'
+      # visibility << 'RIGHT'
     end
+
     biggest_leftward = trees[y][...(x)].max || 0
-    p biggest_leftward
     if tree > biggest_leftward
       is_visible = true
-      visibility << 'LEFT'
+      # visibility << 'LEFT'
     end
+
     biggest_upward = trees[...(y)].map{_1[x]}.max || 0
-    p biggest_upward
     if tree > biggest_upward
       is_visible = true
-      visibility << 'UP'
+      # visibility << 'UP'
     end
+
     biggest_downward = trees[(y+1)..].map{_1[x]}.max || 0
-    p biggest_downward
     if tree > biggest_downward
       is_visible = true
-      visibility << 'DOWN'
+      # visibility << 'DOWN'
     end
+
     if x == 0 || x == (w - 1) || y == 0 || y == (h - 1)
       is_visible = true
     end
 
     if is_visible
-      puts "#{tree} at <#{x}, #{y}> is visible from #{visibility.join('/')}!"
+      # puts "#{tree} at <#{x}, #{y}> is visible from #{visibility.join('/')}!"
       num_visible += 1
     end
   end
-end
-
-trees.each do |row|
-  puts row.join
 end
 
 perimeter = 2 * ((w - 1) + (h - 1))
