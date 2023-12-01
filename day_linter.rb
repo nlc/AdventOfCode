fdc3ddafc951bc2c9959ed0601f598524310ec91
nlc <natechav1@gmail.com>
2023-12-01 00:15:49 -0600

    day_linter.rb

---
+++ day_linter.rb
@@ -0,0 +1,78 @@
+require 'optparse'
+
+STARTING_YEAR = 2015
+CURRENT_YEAR = Time.now.year
+STARTING_DAY = 1
+ENDING_DAY = 25
+
+COMPLAINT_MESSAGES = {
+  missing: 'folder is missing',
+  empty: 'folder is empty',
+  nopuzzle: 'lacks puzzle.txt',
+  noinput: 'lacks input.txt',
+  noscript: 'lacks properly-named script'
+}
+
+def parse_contents(path, contents)
+  complaints = []
+
+  if contents.empty?
+    complaints << :empty
+  else
+    if !contents.include?('puzzle.txt')
+      complaints << :nopuzzle
+    end
+
+    if !contents.include?('input.txt')
+      complaints << :noinput
+    end
+
+    if !contents.grep(/^ab?\./).any?
+      complaints << :noscript
+    end
+  end
+
+  complaints
+end
+
+options = {}
+opt_parser =
+  OptionParser.new do |opts|
+    opts.on('-m', '--report-missing', 'Also report when a folder is missing') { |m| options[:report_missing] = true }
+  end
+opt_parser.parse!
+
+(STARTING_YEAR..CURRENT_YEAR).each do |year|
+  year_days =
+    (STARTING_DAY..ENDING_DAY).map do |day|
+      folder_path = "./#{year}/Day#{day}"
+      folder_path = sprintf('./%d/Day%02d', year, day)
+
+      complaints =
+        if Dir.exist?(folder_path)
+          contents = Dir.glob("#{folder_path}/*").map { |full_path| File.basename(full_path) }
+
+          parse_contents(folder_path, contents)
+        else
+          options[:report_missing] ? [:missing] : []
+        end
+
+      [day, complaints]
+    end.to_h
+
+  unless year_days.nil?
+    puts "#{year}:"
+    year_days.compact.each do |day, complaints|
+      case complaints.length
+      when 0
+      when 1
+        printf("  Day %2d: %s\n", day, COMPLAINT_MESSAGES[complaints.first])
+      else
+        printf("  Day %2d:\n", day)
+        complaints.each do |complaint|
+          puts "    #{COMPLAINT_MESSAGES[complaint]}"
+        end
+      end
+    end
+  end
+end
